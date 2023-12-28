d15e33b3de3812a49d3cd3978e205386774ed244
Danthie <114122786+Danthiee@users.noreply.github.com>
2023-12-27 21:42:22 -0600

    ok

--- Facebook-phake/Component/imgupload.js
+++ Facebook-phake/Component/imgupload.js
@@ -1,7 +1,12 @@
 import react from React;
+import avt from './img/avt.jpg';
 
 function Upload () {
     return(
-        
-    )
+        <div classname="Upload">
+           <img src="avt" />
+
+        </div>
+    );
 }
+export default Upload
